3c3df9d2726c4555cdc20c05a9058489c739946c
willparr <wparr17@gmail.com>
2021-03-03 20:30:16 -0600

    pushing for pull

--- src/screens/distance.tsx
+++ src/screens/distance.tsx
@@ -5,65 +5,33 @@ import { FlatList } from 'react-native';
 import {
   Box, Button, Paragraph, Title,
 } from '../providers/theme';
-import { useLocationData, useLocationDistance, useLocationTracking } from '../services/location';
+import { useLocationData, useLocationTracking } from '../services/location';
 
 export const DistanceScreen: React.FC = () => {
   const locations = useLocationData();
   const tracking = useLocationTracking();
-  const distance = useLocationDistance(locations);
+
+  const startPoint = locations[0];
 
   return (
     <Box variant='page'>
       <Box>
-        <Title>Your office marathon</Title>
-        {distance === 0
-          ? <Paragraph>You didn't walk yet. Start location tracking and get walking!</Paragraph>
-          : <Paragraph>You walked {distance} meters! Keep it up!</Paragraph>
-        }
-      </Box>
-      <Box variant='row'>
+        <Title>Track Setup</Title>
         {tracking.isTracking
-          ? <Button onPress={tracking.stopTracking}>Pause tracking</Button>
-          : <Button onPress={tracking.startTracking}>Start tracking</Button>
+          ? <Paragraph>Calibrating...</Paragraph>
+          : <Paragraph>Press start and start driving.</Paragraph>
         }
-        <Button variant='primary' onPress={tracking.clearTracking}>Reset data</Button>
       </Box>
-      <DistanceLocationList locations={locations} />
+      <Box variant='column'>
+        {tracking.isTracking
+          ? <Button onPress={tracking.stopTracking}>Reset Calibration</Button>
+          : <Button onPress={tracking.startTracking}>Set Start Point</Button>
+        }
+        <Button variant='primary' onPress={tracking.clearTracking}>Reset Calibration</Button>
+        <Paragraph>Lat: {startPoint?.coords?.latitude}</Paragraph>
+        <Paragraph>Lng: {startPoint?.coords?.longitude}</Paragraph>
+      </Box>
     </Box>
   );
 };
 
-const DistanceLocationList: React.FC<{ locations: LocationObject[] }> = (props) => {
-  const listRef = useRef<FlatList<LocationObject>>(null);
-
-  useEffect(() => {
-    // don't ask... if we call it directly,
-    // the list scrolls to the "previous" end instead of the "new" end
-    setTimeout(() => listRef.current?.scrollToEnd());
-  }, [props.locations.length]);
-
-  return (
-    <Box>
-      <FlatList
-        ref={listRef}
-        style={{ flexGrow: 0, flexBasis: 200 }}
-        data={props.locations}
-        keyExtractor={(location, index) => `${location.timestamp}-${index}`}
-        renderItem={(entry) => (
-          <DistanceLocation
-            number={entry.index}
-            location={entry.item}
-          />
-        )}
-      />
-    </Box>
-  );
-};
-
-const DistanceLocation: React.FC<{ number: number, location: LocationObject }> = (props) => (
-  <Box variant='row'>
-    <Paragraph>#{props.number + 1}</Paragraph>
-    <Paragraph>lat: {props.location.coords.latitude}</Paragraph>
-    <Paragraph>lng: {props.location.coords.longitude}</Paragraph>
-  </Box>
-);
